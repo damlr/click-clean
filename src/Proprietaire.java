6fd7605912718109a16fafd0d88186ba2220dbb3
Stagiaire Tempora <152262862+damlr@users.noreply.github.com>
2024-01-29 16:09:00 +0100

    update
    tools update

---
+++ src/Proprietaire.java
@@ -0,0 +1,21 @@
+package com.click_and_clean;
+
+import java.util.HashMap;
+
+/**
+ * Classe de demandeur de menage, implementer un vector de Propriete quand la
+ * classe sera faites
+ * 
+ * @author Simon
+ */
+public class Proprietaire extends Utilisateur {
+
+	public Proprietaire(String username, String firstName, String secondName, int age, String password,
+			String profilePicture, String email, String description, String phoneNumber, String dateOfBirth,
+			String dateOfCreation, HashMap<Float, String> comment, float globalGrade) {
+		super(username, firstName, secondName, age, password, profilePicture, email, description, phoneNumber,
+				dateOfBirth,
+				dateOfCreation, comment, globalGrade);
+		// TODO Auto-generated constructor stub
+	}
+}
